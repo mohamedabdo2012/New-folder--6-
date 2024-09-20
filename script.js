d0e181b0ed7ccc528975aa3d7e047da2fec3dd8a
mohamedabdo2012 <160782295+mohamedabdo2012@users.noreply.github.com>
2024-09-20 15:39:32 +0300

    Mmm

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
 const noteInput = document.getElementById("noteInput");
+const notesDiv = document.getElementById("notesDiv");
 var notesArray = [];
 
 
@@ -11,6 +11,7 @@ function addNote() {
         notesArray.push(newNote);
         noteInput.value = "";
         saveNotes();
+        showNotes();
         console.log(notesArray);
     }
 }
@@ -28,3 +29,30 @@ function getNotes() {
         notesArray = JSON.parse(notesString);
     }
 }
+
+function showNotes() {
+    
+    getNotes();
+
+    notesDiv.innerHTML = "";
+
+    for (let index = 0; index < notesArray.length; index++) {
+        const element = notesArray[index];
+        
+        const newDiv = document.createElement("div");
+
+        const newParagraph = document.createElement("p");
+        newParagraph.innerText = element;
+        newDiv.appendChild(newParagraph);
+
+        const deleteIcon = document.createElement("i");
+        deleteIcon.className = "bi bi-trash3";
+        newDiv.appendChild(deleteIcon);
+
+        notesDiv.appendChild(newDiv);
+
+    }
+
+}
+
+showNotes();
